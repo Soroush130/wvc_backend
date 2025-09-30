22b6a5217f4e5b6558fcc893a4b54e18b7c37300
Soroush130 <soroosh1381amir@gmail.com>
2025-09-30 20:55:19 +0330

    fix:Changed ALLOWED_HOSTS into base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.95.220.11', 'localhost', '127.0.0.1']
 
 # Application definition
 
