1c98d6a5db156e4f41e7330bd2c6ed13ac1f5da0
Soroush130 <soroosh1381amir@gmail.com>
2025-10-01 16:49:24 +0330

    Updated base.py

--- core/settings/development.py
+++ core/settings/development.py
@@ -4,14 +4,3 @@ DEBUG = True
 
 # Cors
 # CORS_ALLOW_ALL_ORIGINS = True
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
-    }
-}
